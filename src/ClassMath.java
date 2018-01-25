8a36bd858b4bd303587545783da87c90ef7dcdbc
Alex <alexshox@gmail.com>
2018-01-24 22:42:06 -0200

    Classe Math
    Change-Id: If48e2ecef917e4d51d15181452eaf8707e60ec42
    Signed-off-by: Alex <alexshox@gmail.com>

---
+++ src/ClassMath.java
@@ -0,0 +1,21 @@
+// A Classe Match retorna uma infinidade de operações matemáticas abaixo alguns métodos.  
+public class ClassMath {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		//Elementos Classe!
+		System.out.println(Math.sin(10)); // Seno
+		System.out.println(Math.cos(10)); // Cosseno
+		System.out.println(Math.tan(10)); // Tangente
+		System.out.println(Math.pow(3, 3)); //Potência
+		System.out.println(Math.sqrt(81)); //raiz quadrada 
+		System.out.println(Math.cbrt(16)); //raiz cúbica
+		System.out.println(Math.hypot(6, 8)); //Hipotenusa
+		
+		
+	}
+
+}
