bc2721e8554ed5d2a4245fa7fb09fc28f3c7e1a9
Alex <alexshox@gmail.com>
2018-01-30 22:56:13 -0200

    Trabalahndo com Java Doc

--- src/ClassesAninhada.java
+++ src/ClassesAninhada.java
@@ -1,25 +1,42 @@
 public class ClassesAninhada {
 
+	/**
+	 * Uma documentação de exemplo <code> Exemplo </code>
+	 * Deve se adicionar uma breve descrição do código fonte.
+	 * 
+	 * @author Alex Junio
+	 * @author Alex Silva
+	 * @version 1.0
+	 * @since 1.0 
+	 * 
+	 * @see java.lang
+	 * @see java.lang
+	 *
+	 */
+
 	private String texto = "texto externo";
-	
-	public class Interna{
-		
+
+	public class Interna {
+
 		private String texto = "texto interno";
-		
-		public void imprimeTexto(){
+
+		public void imprimeTexto() {
 			System.out.println(texto);
-			
-			//consegue ver membros da classe externa
-			System.out.println(ClassesAninhada.this.texto); 
+
+			// consegue ver membros da classe externa
+			System.out.println(ClassesAninhada.this.texto);
 		}
 	}
-	
-	
-	public static void main(String[] args){
-		
+
+	/**
+	 * anotação de método deve ter uma breve descrição sobre sua execução.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
 		ClassesAninhada externa = new ClassesAninhada();
 		Interna interna = externa.new Interna();
-		
+
 		interna.imprimeTexto();
 	}
 }
