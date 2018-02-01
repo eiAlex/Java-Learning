2ef9da05893fc2c17a93da1b4dd54c2967781756
Alex <alexshox@gmail.com>
2018-01-31 22:16:11 -0200

    Trabalhando com properties, Console e RuntimeExec

---
+++ src/Sistemas.java
@@ -0,0 +1,26 @@
+import java.io.Console;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
+
+public class Sistemas {
+
+	public static void main(String[] args) throws IOException {
+
+		Properties p = System.getProperties();
+		System.out.println(p.getProperty("java.version"));
+		p.setProperty("teste.test", "1.0.0");
+		
+		Set<Object> pk = p.keySet();
+		for (Object key : pk) {
+			System.out.println(key + " = "+p.get(key));
+		}
+		
+		Console c = System.console(); // Útil para aplicações que interagem com prompt de comando 
+		
+		Runtime.getRuntime().exec("notepad");  // executa uma aplicação do SO
+		
+		
+	}
+
+}
