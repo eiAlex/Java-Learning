8be13fd4bee3952e1805872a0dcd612c79c8164e
Alex <alexshox@gmail.com>
2018-02-02 23:58:24 -0200

    Junit

---
+++ src/ArquivoTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ArquivoTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
