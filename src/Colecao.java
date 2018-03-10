2c9dc1a6bd058f1822eb2e8e887c659a1a2314de
Álex Junio <alexshox@gmail.com>
2018-03-09 22:59:13 -0300

    Update Colecao.java

--- src/Colecao.java
+++ src/Colecao.java
@@ -6,8 +6,8 @@ public class Colecao {
 
 	public static void main(String[] args) {
 
-		Collection<String> c = new ArrayList<>(); // Instância de Collection onde se é preciso definir o tipo de objeto
-													// onde será implementado.
+		Collection<String> c = new ArrayList<>(); // InstÃ¢ncia de Collection onde se Ã© preciso definir o tipo de objeto
+													// onde serÃ¡ implementado.
 
 		c.add("A"); // Adiciona a uma list
 		c.add("b");
@@ -17,23 +17,24 @@ public class Colecao {
 
 		System.out.println(c.toString()); // converte para String
 		System.out.println(c.isEmpty()); //verifica se esta vazio
-		System.out.println(c.contains("A")); //verifica se contem ocorrência de  um determinado elemento.
+		System.out.println(c.contains("A")); //verifica se contem ocorrÃªncia de  um determinado elemento.
 
 		c.remove("A"); // remove um elemento
 
+		
 		System.out.println(c.toString());
 
 		//trabalhar com grupos de elementos
 		
 		Collection<String> c2 = Arrays.asList("F","G");
 		
-		c.addAll(c2); // o método recebe todos os elementos de outra collection
+		c.addAll(c2); // o mÃ©todo recebe todos os elementos de outra collection
 		
 		System.out.println(c.toString()); //resultado dos dois elementos juntos 
 		
 		System.out.println(c.containsAll(c2)); //Verifica se existem todos os elemento da collection C2 em C
 		
-		System.out.println(c.removeAll(c2)); // remove todos elementos de uma coleção em outra.
+		System.out.println(c.removeAll(c2)); // remove todos elementos de uma coleÃ§Ã£o em outra.
 		
 		
 		// Percorrer elementos de uma collection
@@ -42,7 +43,7 @@ public class Colecao {
 			System.out.print(string);
 		}
 		
-		//converter uma coleção em um array
+		//converter uma coleÃ§Ã£o em um array
 		String[] s = c.toArray(new String[c.size()]);
 		System.out.println(Arrays.toString(s));
 		
