310ead79db258aa4fda681f3129b1400b15b777a
Álex Junio <alexshox@gmail.com>
2019-04-21 11:19:30 -0300

    Create Operadores.java
    Operadores

---
+++ src/Operadores.java
@@ -0,0 +1,26 @@
+//Operadores
+
+public class Operadores {
+
+	public static void main(String[] args) {
+	
+/* OPERADOR								FINALIDADE
+--------------------------------------------------
+ +,-,*,/,% 				soma, subtrai, multiplica, divide, modulo
+ + 						concatena strings
+ +, - 					operador unário mais e menos
+<,>,<=,=> 				comparador, menor que, maior que, menor ou igual que, maior ou igual que
+++,-- 					pré ou pós incremento, pré ou pós decremento
+==,!= 					compara se é igual que ou diferente
+&&, || 					concatenação lógica e, concatenação 
+. 						Acessar propiedades de um obejeto
+[] 						indexar arrays   
+() 						Chamar método
+?: 						ternário, retorna booleano em uma expreção
+*/
+		
+		
+		
+	}
+
+}
