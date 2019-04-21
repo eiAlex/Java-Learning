ff6257d666e4f755e4a97159091bb29589cffad9
Álex Junio <alexshox@gmail.com>
2019-04-21 11:30:04 -0300

    HelloWorld      Este software foi feito para cumprir a tradição do ola mundo. =/     This software was made to fulfill the tradition of the hello world. = /
    HelloWorld
    
        Este software foi feito para cumprir a tradição do ola mundo. =/
        This software was made to fulfill the tradition of the hello world. = /

---
+++ src/helloworld.java
@@ -0,0 +1,28 @@
+/**
+ * Este software foi feito para cumprir a tradição do ola mundo. =/
+ * 
+ * This software was made to fulfill the tradition of the hello world. = /
+ * 
+ */
+package com.spoticode.entity;
+
+/**
+ * @author Alex junio
+ * @version 1.2
+ */
+public class helloworld {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		//attributes
+		 String  ola = "Ola mundo, Hello world o/";
+		 
+		 //output
+		 System.out.println(ola);
+	
+	}
+
+}
